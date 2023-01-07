70c92b2cf527c724fdc3d0a6ab4354e7be6b9a0f
Emilien Loret <emilien.loret@epfedu.fr>
2023-01-07 13:41:55 +0100

    Finalisation de la méthode AfficherMatriceSurConsole Commentaire sur cette même méthode

--- src/speedclick_loret_crettiez/grille.java
+++ src/speedclick_loret_crettiez/grille.java
@@ -19,9 +19,9 @@ public class grille {
     Bouton [][] matrice;  //On appellera notre grille de jeu intéractive, la matrice, et elle sera composé d'objet de type bouton
 
     
-    public grille (int lig, int col) {  //Création d'une grille de taille donnée en paramètre
-        ligne = lig;
-        colonne =  col;
+    public grille (int a, int b) {  //Création d'une grille de taille donnée en paramètre
+        ligne = a;
+        colonne =  b;
         for (int i = 0; i <= ligne; i++) {
             for (int j = 0; j <= colonne; j++) {
                 matrice[i][j] = new Bouton();
@@ -58,20 +58,23 @@ public class grille {
             
             
     public void UtilisationCase () {
-        matrice[ligne][colonne].activation();
+        matrice[ligne][colonne].afficherBouton();
         matrice[ligne][colonne].disparaitreBouton();
     }
     
-    public void afficherMatriceSurConsole(int a, int b) {
+    public void afficherMatriceSurConsole(int a, int b) { //Méthode permettant d'afficher la matrice sur la console avec le bouton allumé
         ligne = a;
         colonne =  b;
-        for (int i=0;i<=ligne;i++) {
+        for (int i=0;i<=ligne;i++) { //On print toutes les colonnes d'une même ligne avec le caractères correspondant (O si y'a rien, X si y'a le bouton, pour ça on a une méthode tostring))
             for (int j=0;j<=colonne;j++) {
                 System.out.print(matrice[i][j]+" ");
             }
-        
+            System.out.println(" "+i+1); //à la fin de chaque ligne, on affiche l'indice de cette même ligne
         }
-            
+        for (int i=0;i<=colonne;i++){ //On veut aussi afficher l'indice des colonnes donc après avoir print la matrice et les indices de ligne
+            int cpt=i+1;
+            System.out.print(cpt+" "); //On va print les indices des b colonnes (b donne le nombre de colonne de la matrie, choisi par l'utilisateur)
+        }
     }
     
 
