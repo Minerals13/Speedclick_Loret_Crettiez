aec70218fabef7494a14eff247e884b25d3ce2cf
Emilien Loret <emilien.loret@epfedu.fr>
2023-01-08 12:10:14 +0100

    Ajustement sur les paramètres de certaines méthodes Réglage du bug qui bloquait l'affiche de la grille sur console

--- src/speedclick_loret_crettiez/grille.java
+++ src/speedclick_loret_crettiez/grille.java
@@ -16,14 +16,15 @@ import java.util.Random;
 public class grille {
     int ligne;
     int colonne;
-    Bouton [][] matrice;  //On appellera notre grille de jeu intéractive, la matrice, et elle sera composé d'objet de type bouton
+    Bouton[][] matrice;  //On appellera notre grille de jeu intéractive, la matrice, et elle sera composé d'objet de type bouton
 
     
-    public grille (int a, int b) {  //Création d'une grille de taille donnée en paramètre
+    public grille (int a, int b) {  //Création d'une grille de taille donnée en paramètre. Toutes les cases sont remplis d'un objet de type Bouton
+        matrice = new Bouton[a][b];
         ligne = a;
         colonne =  b;
-        for (int i = 0; i <= ligne; i++) {
-            for (int j = 0; j <= colonne; j++) {
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
                 matrice[i][j] = new Bouton();
             }
         }        
@@ -43,7 +44,7 @@ public class grille {
         int TailleLigne=a;
         Random random = new Random(); 
         int LigneON;
-        LigneON = random.nextInt(TailleLigne+1);         //On choisit un entier entre 0 et la taille de la matrice+1. Cette taille est donnée par l'utilisateur en entrée de programme      
+        LigneON = random.nextInt(TailleLigne);         //On choisit un entier entre 0 et la taille de la matrice+1. Cette taille est donnée par l'utilisateur en entrée de programme      
     return LigneON; //On retourne l'indice de ligne du bouton allumé
     
     }
@@ -52,16 +53,16 @@ public class grille {
         int TailleColonne=b;
         Random random = new Random();
         int ColonneON;
-        ColonneON = random.nextInt(TailleColonne+1);
+        ColonneON = random.nextInt(TailleColonne);
     return ColonneON; //On retourne l'indice de colonne du bouton allumé
     }
             
             
-    public void afficherMatriceSurConsole(int a, int b) { //Méthode permettant d'afficher la matrice sur la console avec le bouton allumé
+    public void afficherMatriceSurConsole(int a, int b) { //Méthode permettant d'afficher la matrice sur la console avec le bouton allumé; a et b correspondent à la taille de la matrice
         ligne = a;
         colonne =  b;
-        for (int i=0;i<=ligne;i++) { //On print toutes les colonnes d'une même ligne avec le caractères correspondant (O si y'a rien, X si y'a le bouton, pour ça on a une méthode tostring))
-            for (int j=0;j<=colonne;j++) {
+        for (int i=0;i<ligne;i++) { //On print toutes les colonnes d'une même ligne avec le caractères correspondant (O si y'a rien, X si y'a le bouton, pour ça on a une méthode tostring))
+            for (int j=0;j<colonne;j++) {
                 System.out.print(matrice[i][j]+" ");
             }
             System.out.println(" "+i+1); //à la fin de chaque ligne, on affiche l'indice de cette même ligne
