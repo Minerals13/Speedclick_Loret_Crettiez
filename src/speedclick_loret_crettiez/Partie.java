c7f855ed95593a34e599157f69a3a9e11fb1dea8
Emilien Loret <emilien.loret@epfedu.fr>
2023-01-08 03:46:55 +0100

    Modification et finalisation méthode LancerPartie
    Permets de :
    Déterminer un nouveau bouton à chaque manche
    Faire un nombre x de manche
    Verifier que les coordonnées données par l'utilisateurs sont les mêmes que celles crées
    Compter les points de l'utilisateur en fonction de ses réponses
    Afficher la nouvelle grille à chaque fois avec le nouveau bouton
    Permets à l'utilisateur d'avoir 3 chances, sinon il devra recommencer à 0

--- src/speedclick_loret_crettiez/Partie.java
+++ src/speedclick_loret_crettiez/Partie.java
@@ -19,10 +19,10 @@ public class Partie {
         
     }
 
-    public void LancerPartie (int TailleLigne, int TailleColonne) { 
+    public void LancerPartie (int TailleLigne, int TailleColonne) {  //méthode qui permets de jouer à une partie classique. Dedans on détermine des nouvelles coordonnées de bouton à chaque fois qu'il est trouvé, et on affiche la nouvelle grille à chaque fois        
         int a = TailleLigne;
         int b = TailleColonne;
-        int cpt =0;
+        int cpt =0; //C'est le compteur pour le nombre de point de l'utilisateur
         Scanner scan = new Scanner(System.in);
         
         
@@ -31,33 +31,55 @@ public class Partie {
             int LigneON=Plateau.LigneAllumee(a); //on appelle la méthode LigneAllumée pour déterminer quel indice de ligne notre bouton va prendre
             int ColonneON=Plateau.ColonneAllumee(b); //Pareil mais pour la colonne
         
-            Plateau.PlacerBouton(LigneON, ColonneON); //On place notre bouton fraichement déterminé, sur la grille
+            Plateau.PlacerBouton(LigneON, ColonneON); //On place notre bouton de coordonnées déterminées juste au dessus , sur la grille
             Plateau.afficherMatriceSurConsole(a, b); //On affiche la grille avec le bouton
             
             System.out.println("Veuillez choisir la ligne du bouton allumé"); 
             int ChoixLigne = scan.nextInt(); //On demande à l'utilisateur l'indice de la ligne de la case dans laquelle le bouton allumé
             System.out.println("Veuillez choisir la colonne du bouton allumé");  
             int ChoixColonne = scan.nextInt(); //même chose mais pour les colonnes
+                
             
-            
-            while ( (Plateau.PresenceBouton(ChoixLigne,ChoixColonne)!=Plateau.PresenceBouton(LigneON,ColonneON))) {
+             //On regarde si les coordonnées données par l'utilisateurs sont les mêmes que celles du bouton
+             //On donne 3 chances, au bout du troisème raté le compteur redescend à 0
+             //A chaque erreure d'affilée on rentre dans un nouveau if
+             //Si les coordonnées sont bonnes, on rentre dans le else if correspondnt au 1er, 2e ou 3e essai. On donne alors un point à l'utilisateur et on fait disparaitre le bouton pour la prochaine manche
+            if ( (Plateau.PresenceBouton(ChoixLigne,ChoixColonne)!=Plateau.PresenceBouton(LigneON,ColonneON))) {    //Premier essai
+                System.out.println("Raté, reessaye"); 
                 System.out.println("Veuillez choisir la ligne du bouton allumé"); 
-                int ChoixLigne = scan.nextInt(); //On demande à l'utilisateur l'indice de la ligne de la case dans laquelle le bouton allumé
+                ChoixLigne = scan.nextInt(); 
                 System.out.println("Veuillez choisir la colonne du bouton allumé");  
-                int ChoixColonne = scan.nextInt(); //même chose mais pour les colonnes
+                ChoixColonne = scan.nextInt(); 
                 
                 
-                cpt=cpt+1;  //Si c'est la bonne case, l'utilisateur gagne un point
+                if ( (Plateau.PresenceBouton(ChoixLigne,ChoixColonne)!=Plateau.PresenceBouton(LigneON,ColonneON))) {    //Deuxième essai 
+                    System.out.println("Encore Raté, reessaye");
+                    System.out.println("Veuillez choisir la ligne du bouton allumé"); 
+                    ChoixLigne = scan.nextInt(); 
+                    System.out.println("Veuillez choisir la colonne du bouton allumé");  
+                    ChoixColonne = scan.nextInt(); 
                 
-                
-                Plateau.DisparitionBouton(LigneON,ColonneON); //Et on fait disparaitre le bouton
-            }else 
-                
+                    
+                    if ( (Plateau.PresenceBouton(ChoixLigne,ChoixColonne)!=Plateau.PresenceBouton(LigneON,ColonneON))) {    //Troisième essai 
+                        System.out.println("Tu as fais trop d'erreur à la suite, recommence");
+                        cpt=0; 
+                    } else if  (Plateau.PresenceBouton(ChoixLigne,ChoixColonne)==Plateau.PresenceBouton(LigneON,ColonneON)) { //C'est validé au 3e essai
+                    cpt+=1; //Si c'est la bonne case, l'utilisateur gagne un point
+                    Plateau.DisparitionBouton(LigneON, ColonneON); } //Et on fait disparaitre le bouton
+                    
+                    
+                }else if  (Plateau.PresenceBouton(ChoixLigne,ChoixColonne)==Plateau.PresenceBouton(LigneON,ColonneON)) { //C'est validé au 2e essai
+                    cpt+=1;
+                    Plateau.DisparitionBouton(LigneON, ColonneON); }
+                    
+                    
+            }else if  (Plateau.PresenceBouton(ChoixLigne,ChoixColonne)==Plateau.PresenceBouton(LigneON,ColonneON)) { //C'est validé au 1er essai
+                cpt+=1;
+                Plateau.DisparitionBouton(LigneON, ColonneON);       
             }    
             
-            
-            
-            
+        //Puis on recommence avec des nouvelles coordonnées, jusqu'à ce qu'on ait 15 points
+  
         }
         
     }
