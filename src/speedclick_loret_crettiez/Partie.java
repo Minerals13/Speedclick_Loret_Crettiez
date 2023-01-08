4f5283e74e798defda4c6d2cfc646c1e3ba0770a
Emilien Loret <emilien.loret@epfedu.fr>
2023-01-08 18:03:45 +0100

    Finalisation classe partie
    On a une chance sur 10 à chaque début de tour qu'il y ait un bouton piégé

--- src/speedclick_loret_crettiez/Partie.java
+++ src/speedclick_loret_crettiez/Partie.java
@@ -4,6 +4,7 @@
  */
 package speedclick_loret_crettiez;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -30,17 +31,23 @@ public class Partie {
         while (cpt < NombrePoints) {  
             int LigneON=Plateau.LigneAllumee(a); //on appelle la méthode LigneAllumée pour déterminer quel indice de ligne notre bouton va prendre
             int ColonneON=Plateau.ColonneAllumee(b); //Pareil mais pour la colonne
-            int BadLigne=Plateau.LigneAllumeeMauvais(a);
-            int BadColonne=Plateau.ColonneAllumeeMauvais(b);
+            ;
             
             
             Plateau.PlacerBouton(LigneON, ColonneON); //On place notre bouton de coordonnées déterminées juste au dessus , sur la grille
             Plateau.afficherMatriceSurConsole(a, b); //On affiche la grille avec le bouton avec en paramètre la taille de la matrice
-            Plateau.PlacerMauvaisBouton(BadLigne, BadColonne);
             
-            System.out.println("Veuillez choisir la ligne du bouton allume"); 
+            Random random = new Random();
+            int Proba = random.nextInt(9); //Chiffre entre 0 et 9
+            if (Proba==5) { //On a une chance sur 10 de placer une bouton piégé
+                int BadLigne=Plateau.LigneAllumeeMauvais(a);
+                int BadColonne=Plateau.ColonneAllumeeMauvais(b);   
+                Plateau.PlacerMauvaisBouton(BadLigne, BadColonne);
+            }
+            
+            System.out.println("1er essai : Veuillez choisir la ligne du bouton allume"); 
             int ChoixLigne = scan.nextInt(); //On demande à l'utilisateur l'indice de la ligne de la case dans laquelle le bouton allumé
-            System.out.println("Veuillez choisir la colonne du bouton allume");  
+            System.out.println("1er essai : Veuillez choisir la colonne du bouton allume");  
             int ChoixColonne = scan.nextInt(); //même chose mais pour les colonnes
                 
             
@@ -51,18 +58,18 @@ public class Partie {
             if ((Plateau.PresenceBouton2(ChoixLigne - 1,ChoixColonne - 1)!=Plateau.PresenceBouton(LigneON,ColonneON)) && (Plateau.PresenceBouton2(ChoixLigne-1, ChoixColonne-1)!=Plateau.PresenceMauvaisBouton(BadLigne,BadColonne))) {    //Premier essai. On vérifie que le bouton choisie n'est ni le bon ni le mauvais
                 System.out.println("Rate, plus que 2 essais"); //Ce n'est pas le bon bouton mais ce n'est pas le piège non plus, l'utilisateur pour re-essayer
                 Plateau.afficherMatriceSurConsole(a, b); //On réaffiche la grille
-                System.out.println("Veuillez choisir la ligne du bouton allumé"); 
+                System.out.println("2e essai : Veuillez choisir la ligne du bouton allumé"); 
                 ChoixLigne = scan.nextInt(); 
-                System.out.println("Veuillez choisir la colonne du bouton allumé");  
+                System.out.println("2e essai : Veuillez choisir la colonne du bouton allumé");  
                 ChoixColonne = scan.nextInt(); 
                 System.out.println("Tu as "+cpt+" points"); //On affiche le nombre de point à chaque tour
                 
                 if (( (Plateau.PresenceBouton2(ChoixLigne - 1,ChoixColonne - 1)!=Plateau.PresenceBouton(LigneON,ColonneON))) && (Plateau.PresenceBouton2(ChoixLigne-1, ChoixColonne-1)!=Plateau.PresenceMauvaisBouton(BadLigne,BadColonne))) {    //Deuxième essai, même fonctionnement que le premier, il gagne un point ou perds un point 
                     System.out.println("Encore Rate, plus que 1 essai");
                     Plateau.afficherMatriceSurConsole(a, b);
-                    System.out.println("Veuillez choisir la ligne du bouton allumé"); 
+                    System.out.println("3e essai : Veuillez choisir la ligne du bouton allumé"); 
                     ChoixLigne = scan.nextInt(); 
-                    System.out.println("Veuillez choisir la colonne du bouton allumé");  
+                    System.out.println("3e essai :Veuillez choisir la colonne du bouton allumé");  
                     ChoixColonne = scan.nextInt(); 
                     
                     
@@ -105,8 +112,12 @@ public class Partie {
                         Plateau.DisparitionMauvaisBouton(BadLigne, BadColonne); //Et on fait disparaitre le bouton piègé
                         System.out.println("Vous êtes tombé dans le piège, vous perdez un point");
                         Plateau.DisparitionBouton(LigneON, ColonneON);
-            }   
-            
+            }   else if ((Plateau.PresenceBouton2(ChoixLigne - 1,ChoixColonne - 1)==Plateau.PresenceBouton(LigneON, ColonneON)) && (Plateau.PresenceBouton2(ChoixLigne-1, ChoixColonne-1)==Plateau.PresenceMauvaisBouton(BadLigne,BadColonne))) {
+                 cpt+=1;
+                System.out.println("Vous avez trouve le bon bouton");
+                Plateau.DisparitionBouton(LigneON, ColonneON);  
+                Plateau.DisparitionMauvaisBouton(BadLigne, BadColonne);  
+            }
         //Puis on recommence avec des nouvelles coordonnées, jusqu'à ce qu'on ait 15 points
   
         }
